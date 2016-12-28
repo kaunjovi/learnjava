da0571654db150d4cbfb07375f24245ab0267696
kaunjovi <kaunjovi@gmail.com>
2016-12-28 19:31:39 +0530

    First failing junit

---
+++ src/test/java/le/arn/TestJUnitSetup.java
@@ -0,0 +1,14 @@
+package le.arn;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestJUnitSetup {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
